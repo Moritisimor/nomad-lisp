49a898c6042a17bbf54a7fd7c2c89f5ecb958b67
Moritisimor <devMoritisimor@proton.me>
2026-08-01 01:03:51 +0200

    replaced prerr with print

--- lib/nomad_err.ml
+++ lib/nomad_err.ml
@@ -3,8 +3,7 @@ type error =
   | TokenizerError of string
   | EvaluationError of string
 
-let print_err e =
-  match e with
+let print_err = function
   | ParseError e -> print_endline ("Error while parsing: " ^ e)
   | TokenizerError e -> print_endline ("Error while tokenizing: " ^ e)
-  | EvaluationError e -> prerr_endline ("Error while evaluating: " ^ e)
+  | EvaluationError e -> print_endline ("Error while evaluating: " ^ e)
